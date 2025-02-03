45f672b972cd98f4ef52bc89262b4b955b1145a3
ilkinlev <arcrutus007@gmail.com>
2025-02-04 00:07:58 +0400

    Homework17
    Done

---
+++ Homework17/script.js
@@ -0,0 +1,48 @@
+// 1.
+const myNumber = 1;
+const myString = "Hello world!";
+const myBoolean = true;
+console.log(myString, myNumber, myBoolean);
+
+// 2.
+
+//Deyisen teyin edirik
+
+let testNum = 25;
+
+//console.log() ile browserdeki consolumuza deyeri yazdiririq
+
+console.log("testNum = ", testNum);
+
+/* 
+    Deyisen teyin etdikde diqqet yetirmeli oldugumuz meqamlar var bunlardan biride 'reserved words' 
+    deyilen bir anlayis var ve bu sozlerden biz istifade ede bilmirik bir deyisene 'let, const, byte, boolean ve.s
+    kimi sozlerden istifade oluna bilmez
+*/
+
+// 3.
+
+let a = 5;
+let b = 10;
+
+// Add
+console.log("Add: ", a + b);
+// Subtract
+console.log("Subtract: ", a - b);
+// Multiply
+console.log("Multiply: ", a * b);
+// Divide
+console.log("Divide: ", a / b);
+
+// 4.
+
+let myVariable = "camelCase";
+let myvariable = "normal text";
+
+console.log(myvariable);
+console.log(myVariable);
+
+/* JavaScriptde case-sensitive bir dildir bu o demekdir deyisenlerin identifikatorlari ardicil yazilmalidir 
+   yuxardaki yazdigimizda meselde her ikisinden birisi olmasa idi kodumuz xeta verecekdi ve bele bir degiskenin
+   olmadigini deyecekdi
+*/
