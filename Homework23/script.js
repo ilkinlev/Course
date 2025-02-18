f9e87518c9baa08a19028f84046db5dc156331b9
ilkinlev <arcrutus007@gmail.com>
2025-02-18 14:24:44 +0400

    Homework23
    Done

---
+++ Homework23/script.js
@@ -0,0 +1,23 @@
+// Task 1.
+
+// let userInput = Number(prompt("Write a number: "));
+
+// if (!isNaN(userInput) && userInput >= 0) {
+//   while (userInput >= 0) {
+//     console.log(userInput);
+//     userInput--;
+//   }
+//   console.log("Countdown finished!");
+// } else {
+//   alert("Wrong Input");
+// }
+
+// Task 2.
+
+// for (let i = 1; i <= 10; i++) {
+//   for (let j = 1; j < 10; j++) {
+//     console.log(`${i} x ${j} = ${i * j}`);
+//   }
+// }
+
+//Commentde özüm aldım müellim ağırlıq olmasın deye :)
